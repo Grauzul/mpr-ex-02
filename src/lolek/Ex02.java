63be04b8c39c93bfe1cfc1d522d37795335a0e3d
student <student@virtualbox>
2013-10-13 13:42:20 +0200

    Rozwiazanie

--- src/lolek/Ex02.java
+++ src/lolek/Ex02.java
@@ -1,5 +1,8 @@
 package lolek;
 
+import java.util.List;
+import java.util.ListIterator;
+
 /*
  * a) Zmodyfikuj kod „generatora liczb pierwszych” PrimesGen tak, żeby
  *    zamiast tablic wykorzystywał listy. W szczególności wynikiem metody
@@ -14,9 +17,11 @@ public class Ex02 {
 
 	public static void main(String[] args) {
 		int numOfPrimes = 20;
-		int[] primes = PrimesGen.genPrimes(numOfPrimes);
-		for (int pn : primes) {
-			System.out.printf("%d ", pn);
+		List<Integer> primes = PrimesGen.genPrimes(numOfPrimes);
+		ListIterator<Integer> iterator = primes.listIterator(numOfPrimes);
+		for (int i = 0; i<10; i++) {
+			if(iterator.hasPrevious()){
+			System.out.printf("%d ", iterator.previous());
 		}
 		System.out.println();
 	}
